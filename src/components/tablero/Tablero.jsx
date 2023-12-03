624ce79869af680df364c5587e0453a6426f4c09
Tomás Acosta <tomas23058@gmail.com>
2023-12-03 19:35:22 -0300

    Add tablero numeros y letras

--- src/components/tablero/Tablero.jsx
+++ src/components/tablero/Tablero.jsx
@@ -1,10 +1,27 @@
 import "./tablero.css";
 import Celda from "../celda/Celda";
+import { Typography } from "@mui/material";
 
 export default function Tablero({ tablero, onClickCelda }) {
+  const letras = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
   return (
     <div className="tablero-container">
-      <div className="tablero">
+      <div className=" ">
+        {letras.map((letra) => (
+          <div className="letra" key={letra}>
+            <Typography variant="h5">{letra}</Typography>
+          </div>
+        ))}
+      </div>
+      <div>
+        <div className="fila">
+          {tablero.map((fila) => (
+            <div className="letra" key={fila.nroFila + 1}>
+              <Typography variant="h5">{fila.nroFila + 1}</Typography>
+            </div>
+          ))}
+        </div>
         {tablero.map((fila) => (
           <div key={fila.nroFila} className="fila">
             {fila.celdas.map((celda) => (
