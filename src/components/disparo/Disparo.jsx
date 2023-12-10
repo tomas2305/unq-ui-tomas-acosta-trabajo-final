9c1713b241b07c134c4f3048dfc00d8c1d6dd151
Tomás Acosta <tomas23058@gmail.com>
2023-12-10 20:18:46 -0300

    Add focus en disparo

--- src/components/disparo/Disparo.jsx
+++ src/components/disparo/Disparo.jsx
@@ -1,8 +1,22 @@
 import WaterIcon from "@mui/icons-material/Water";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
+import "./disparo.css";
+import { useEffect, useState } from "react";
 
 export default function Disparo({ esAcertado }) {
+  const [mounted, setMounted] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setMounted(false);
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div className="">{esAcertado ? <WhatshotIcon /> : <WaterIcon />}</div>
+    <div className={`disparo ${mounted ? "disparo-focus" : ""}`}>
+      {esAcertado ? <WhatshotIcon /> : <WaterIcon />}
+    </div>
   );
 }
